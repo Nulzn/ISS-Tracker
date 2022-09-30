3b91ea6918aa7bdc2fa1f66d241f2c01496b4734
Nils R <nils.ritze@icloud.com>
2022-09-30 11:22:42 +0200

    ISS API Done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using System;
+using System.Net.Http;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+
+namespace ISSTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newAPI = new API();
+            newAPI.runISS("https://api.wheretheiss.at/v1/satellites/25544");
+        }
+    }
+
+    public class API
+    {
+        public void runCoordinates()
+        {
+
+        }
+        public void runISS(string path)
+        {
+            using (var client = new HttpClient())
+            {
+                var endpoint = new Uri(path);
+                var result = client.GetAsync(endpoint).Result;
+                var data = result.Content.ReadAsStringAsync().Result;
+                dynamic jsonData = JObject.Parse(data);
+
+
+                Console.WriteLine($"LAT: {jsonData.latitude} | LON: {jsonData.longitude}");
+
+            }
+        }
+    }
+}
